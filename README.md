6c31eb9c46851788ff435d667cb5d92a0374e6a9
pierslabs <pedrolosasp@gmail.com>
2022-05-20 22:38:47 +0200

    beforeEach, afterEach

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# testing-course
-# testing-course
+# Testing Course
+## mocha chai
+
+```
+npm init 
+npm i mocha
+```
+
+1. Very simple structure test
+2. 
--- test.js
+++ test.js
@@ -1,6 +1,22 @@
 const assert = require("assert");
 
 describe("first test", () => {
+  before(() => {
+    console.log("================before");
+  });
+
+  after(() => {
+    console.log("================after");
+  });
+
+  beforeEach(() => {
+    console.log("================beforeEach");
+  });
+
+  afterEach(() => {
+    console.log("================AfterEach");
+  });
+
   context("function tested", () => {
     it("should do something", () => {
       assert.equal(1, 1);
