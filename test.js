984574dcd50d397e473dfff271a6427eb12a6cd5
pierslabs <pedrolosasp@gmail.com>
2022-05-20 22:44:38 +0200

    multipleContext

--- test.js
+++ test.js
@@ -1,31 +1,35 @@
 const assert = require("assert");
 
 describe("first test", () => {
-  before(() => {
-    console.log("================before");
-  });
-
-  after(() => {
-    console.log("================after");
-  });
-
-  beforeEach(() => {
-    console.log("================beforeEach");
-  });
-
-  afterEach(() => {
-    console.log("================AfterEach");
-  });
-
   context("function tested", () => {
+    // before(() => {
+    //   console.log("================before");
+    // });
+
+    // after(() => {
+    //   console.log("================after");
+    // });
+
+    // beforeEach(() => {
+    //   console.log("================beforeEach");
+    // });
+
+    // afterEach(() => {
+    //   console.log("================AfterEach");
+    // });
+
     it("should do something", () => {
       assert.equal(1, 1);
     });
 
-    it("should do something", () => {
+    it("should do something else", () => {
       assert.deepEqual({ name: "Pier" }, { name: "Pier" });
     });
 
     it("this a pending test");
+  });
+
+  context("another function", () => {
+    it("should something 2");
   });
 });
